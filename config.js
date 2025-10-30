4cc8c7aaa7fe4bc0a8a02e24ed5a6caa2fa2d492
yasiya2001 <147257285+yasiya2001@users.noreply.github.com>
2025-10-30 21:25:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?gbVehV'
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?QXZpnO'
 };
